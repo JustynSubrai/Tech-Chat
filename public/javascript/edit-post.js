95830b8b6020853d7f46158832816593578ce339
Justyn Subrai <justynskylarsubrai@gmail.com>
2022-05-19 14:30:15 -0700

    finished product

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,25 +1,9 @@
-async function editFormHandler(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name="post-title"]').value.trim();
-    const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
-    const response = await fetch(`/api/posts/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify({
-        title
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard/');
-    } else {
-      alert(response.statusText);
-    }
+const withAuth = (req, res, next) => {
+  if (!req.session.user_id) {
+      res.redirect('/login');
+  } else {
+      next();
   }
-  
-  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+};
+
+module.exports = withAuth;
